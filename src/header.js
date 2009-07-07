ff752eab62ffc130ca5d31631b3e7723a9d78b3c
Nikolay V. Nemshilov aka St <nemshilov@gmail.com>
2009-07-07 12:02:12 +0400

    build tools were added

---
+++ src/header.js
@@ -0,0 +1,8 @@
+/**
+ * Right-Cards is a playing cards framework implemented with RightJS and CSS
+ *
+ * All the source codes are licensed under the terms of the MIT License
+ *   http://github.com/MadRabbit/right-cards
+ *
+ * @copyright 2009 Nikolay V. Nemshilov aka St.
+ */
